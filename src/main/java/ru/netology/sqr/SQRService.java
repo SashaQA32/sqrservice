649feb15ffd600046929aed7431e078036fe9519
Александр Стрюков <sanyajump16@gmail.com>
2022-02-14 21:59:34 +0300

    initial commit2

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,9 +1,8 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int calculate(int number) {
-        int border1 = 200;
-        int border2 = 300;
+    public int calculate(int border1, int border2) {
+
         int count = 0;
         for (int num = 10; num <= 99; num++) {
             if (num * num >= border1 && num * num <= border2) {
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -9,10 +9,54 @@ class SQRServiceTest {
     @Test
     void shouldCalculate() {
         SQRService service = new SQRService();
+        int border1 = 200;
+        int border2 = 300;
         int expected = 3;
-        int number = 3;
 
-        int actual = service.calculate(number);
+
+        int actual = service.calculate(border1, border2);
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void shouldCalculate1() {
+        SQRService service = new SQRService();
+        int border1 = 100;
+        int border2 = 400;
+        int expected = 11;
+
+
+        int actual = service.calculate(border1, border2);
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void shouldCalculate2() {
+        SQRService service = new SQRService();
+        int border1 = 3500;
+        int border2 = 4100;
+        int expected = 5;
+
+
+        int actual = service.calculate(border1, border2);
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void shouldCalculate3() {
+        SQRService service = new SQRService();
+        int border1 = 8500;
+        int border2 = 12000;
+        int expected = 7;
+
+
+        int actual = service.calculate(border1, border2);
 
         assertEquals(actual, expected);
 
